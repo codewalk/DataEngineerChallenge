bc430818b4b90f0d8e8deb0a40fd3d3322332d9d
swapnil kamthe <swapnilkamthe@gmail.com>
2021-05-20 14:26:10 +0530

    Updated with code formatting. added .gitignore

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Building the JAR
 
-    mvn clean package
+    ## mvn clean package
 * Above command will run test cases regarding reading the file and sessionizing th data
 * Fat jar will be created under target folder which will be used for running the spark application
     
     
     java -cp target\DataChallenge-1.0-SNAPSHOT-jar-with-dependencies.jar com.paypay.Application -Doutput_data_path=D:\projects\DataEngineerChallenge\output -Dinput_data_path=D:\projects\DataEngineerChallenge\data\2015_07_22_mktplace_shop_web_log_sample.log.gz -DENV=LOCAL
     
-* DataChallenge-1.0-SNAPSHOT-jar-with-dependencies.jar => Jar File
+    
+** DataChallenge-1.0-SNAPSHOT-jar-with-dependencies.jar => Jar File
 * com.paypay.Application => Main class
 * output_data_path => Folder to generate the output files
 * input_data_path => Path for the data file.
--- src/main/java/com/paypay/SparkHelper.java
+++ src/main/java/com/paypay/SparkHelper.java
@@ -1,35 +1,25 @@
 package com.paypay;
 
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.expressions.UserDefinedFunction;
-import org.apache.spark.sql.expressions.Window;
-import org.apache.spark.sql.expressions.WindowSpec;
 import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.DecimalType;
-import org.apache.spark.sql.types.StructType;
 
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 
-import static org.apache.spark.sql.functions.*;
-
-;
+import static org.apache.spark.sql.functions.udf;
 
 public class SparkHelper {
 
-	private static SparkSession sparkSession;
+    private static SparkSession sparkSession;
     public static String ENV = System.getProperty("ENV") != null ? System.getProperty("ENV") : "LOCAL";
 
-	public static SparkSession getSparkSession(){
-	    if(sparkSession == null) {
+    public static SparkSession getSparkSession() {
+        if (sparkSession == null) {
 
-	        if(ENV.equalsIgnoreCase("LOCAL")) {
+            if (ENV.equalsIgnoreCase("LOCAL")) {
                 sparkSession = SparkSession.builder()
-                        .appName("SimpleApp") .config("spark.master", "local[*]").getOrCreate();
-            }
-	        else{
+                        .appName("SimpleApp").config("spark.master", "local[*]").getOrCreate();
+            } else {
 
                 sparkSession = SparkSession.builder()
                         .appName("SimpleApp").getOrCreate();
@@ -39,13 +29,8 @@ public class SparkHelper {
 
         }
 
-
-
         return sparkSession;
-
-
-
-        }
+    }
 
     private static void registerUdf() {
         UserDefinedFunction dateConversionUdf = udf(
